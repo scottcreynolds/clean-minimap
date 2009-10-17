4d6be7a25e8085d51b8e7cabf93d7ccfc2ca5621
Scott Reynolds <scottcreynolds@scott-reynoldss-macbook-pro.local>
2009-10-17 14:59:53 -0400

    fixed minimap toggle button

--- KaizenMiniMap.lua
+++ KaizenMiniMap.lua
@@ -1,14 +1,20 @@
 --[[
 	KaizenMiniMap
+	
+	author: scottcreynolds (loganvi of illidan-US)
+	
+	highly opinionated mod about what a clean minimap looks like.
+	if your opinions differ from mine, feel free to change this.
+	
 --]]
 
 local showWorldMapButton = false
 local showZoomButtons = false
 local showCalendar = true
 local showTracking = true
-local showCompass = false
 local showZoneText = true
-local showZoneTextFlair = false
+local showZoneTextBorder = false
+local showMinimapToggleButton = false
 	
 local f = CreateFrame("frame")
 f:SetScript("OnEvent", function(self, event, ...) if self[event] then return self[event](self, event, ...) end end)
@@ -16,32 +22,43 @@ f:RegisterEvent("ADDON_LOADED")
 
 function f:ADDON_LOADED(event, addon)
 	if addon:lower() ~= "kaizenminimap" then return end
+	SetMinimapOptions()
+end
 
-	--refactor to functional
-	if (showZoneTextFlair) then
-		MinimapBorderTop:Show()
-	else
-		MinimapBorderTop:Hide()
-	end
+function SetMinimapOptions()
 	
+	if (showZoneTextBorder) then
+		MinimapBorderTop:SetAlpha(100)
+	else
+		--change alpha rather than hiding the texture - some mods that move bliz UI elements rely on
+		--this to be shown for positioning, so we blend it out but leave it there
+		MinimapBorderTop:SetAlpha(0)
+	end
+
+	if (showMinimapToggleButton) then
+		MinimapToggleButton:Show()
+	else
+		MinimapToggleButton:Hide()
+	end
+
 	if (showZoneText) then
 		MinimapZoneText:Show()
 	else
 		MinimapZoneText:Hide()
 	end
-	
+
 	if (showWorldMapButton) then
 		MiniMapWorldMapButton:Show()
 	else
 		MiniMapWorldMapButton:Hide()
 	end
-	
+
 	if (showTracking) then
 		MiniMapTracking:Show()
 	else
 		MiniMapTracking:Hide()
 	end
-	
+
 	if (showCalendar) then
 		GameTimeFrame:Show()
 	else
@@ -54,11 +71,5 @@ function f:ADDON_LOADED(event, addon)
 	else
 		MinimapZoomIn:Hide()
 		MinimapZoomOut:Hide()
-	end
-	
-	if (showCompass) then
-		MinimapCompassRing:Show()
-	else
-		MinimapCompassTexture:Hide()
 	end
 end
